26232ec5808e489bd36540a93c2af8708f5f9709
Kayce Basques <kayce@google.com>
2020-03-11 17:09:36 -0700

    Add audit logic

--- audit.js
+++ audit.js
@@ -1,6 +1,17 @@
-document.querySelectorAll('img').forEach(img => {
-  if (!img.alt) {
-    console.log(img);
-    img.style.border = '5px solid red';
-  }
-});
+const images = () => {
+  document.querySelectorAll('img').forEach(img => {
+    if (!img.alt) {
+      console.log(img);
+      img.style.border = '5px solid red';
+    }
+  });
+};
+
+const headings = () => {
+  document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(heading => {
+    console.log(heading);
+  });
+};
+
+images();
+headings();
