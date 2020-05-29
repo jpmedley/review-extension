84e63b0a15c7f17e9ff548a33b9e87367fb5cd9f
Kayce Basques <kayce@google.com>
2020-05-29 11:08:56 -0700

    Update view logic

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.browserAction.onClicked.addListener((tab) => {
+  chrome.tabs.executeScript(tab.id, {file: 'content.js'});
+  // chrome.tabs.insertCSS(tab.id, {file: 'content.css'});
+});
--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 (() => {
+  console.log('GREETINGS');
+
   // TODO(kayce): Rather than using sentinels, why not just send data as it's ready?
   // Or provide an overlay within content script that provides the status of each audit?
   const data = {};
@@ -9,33 +11,19 @@
   }
   
   function hero() {
-    sentinels.hero = false;
-    data.hero = {};
     const image = new Image();
     image.addEventListener('load', () => {
       if (image.width !== 3200) {
-        data.hero.ok = false;
-        data.hero.src = image.src;
-        sentinels.hero = true;
-        send();
+        console.log('hero not ok');
+      } else {
+        console.log('hero ok');
       }
     });
     image.src = document.querySelector('.w-hero').src;
-    send();
-  }
-
-  function send() {
-    for (sentinel in sentinels) {
-      if (!sentinel.ready) {
-        console.log(`${sentinel.id} not ready`);
-        break;
-      }
-    }
-    chrome.runtime.sendMessage(data);
   }
 
   // TODO(kayce): Use a custom element, like VisBug?
-  function setup() {
+  function open() {
     document.querySelector('.w-actions').style.display = 'none';
     const content = document.querySelector('.w-post-content');
     content.style.margin = '0 0 0 auto';
@@ -43,17 +31,19 @@
     if (!container) {
       container = document.createElement('iframe');
       container.classList.add('w-extension');
-      // TODO(kayce): Compute the height of the iframe based on the viewport height? Or use a custom element?
-      container.style.width = `${content.getBoundingClientRect().left - 10}px`;
+      container.style.width = `${content.getBoundingClientRect().left}px`;
+      container.style.height = `${document.documentElement.clientHeight}px`;
       container.style.position = 'fixed';
       container.style.top = 0;
-      container.style.bottom = 0;
-      //container.style.backgroundColor = 'black';
-      //container.style.color = 'white';
+      container.style.left = 0;
       container.style.zIndex = '2147483647';
       container.src = chrome.runtime.getURL('content.html');
       document.body.appendChild(container);
     }
+  }
+
+  function setup() {
+    open();
   }
 
   setup();
