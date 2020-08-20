4b2ae8f6cfb416163a038c8351a750c255ee4c08
Kayce Basques <kayce@google.com>
2020-08-20 12:52:05 -0700

    Update README

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
 function links(data) {
+  // LOL. Doesn't work. 
+  // Next try: Inject a script into the page to fetch itself and use response.status.
+  chrome.webNavigation.onErrorOccurred.addListener(data => {
+    // TODO(kaycebasques): Only delete the tab if it's one of the links we opened.
+    // We also need to reduce the URLs to unique pages only. And skip opening same-page links.
+    // chrome.tabs.remove(data.tabId);
+    console.log(`NAV ERROR! ${data}`);
+  });
   chrome.webNavigation.onCompleted.addListener(data => {
     // TODO(kaycebasques): Only delete the tab if it's one of the links we opened.
     // We also need to reduce the URLs to unique pages only. And skip opening same-page links.
     // chrome.tabs.remove(data.tabId);
+    console.log('COMPLET', data);
   });
   data.forEach(link => {
     chrome.tabs.create({url: link, active: false}, tab => {
